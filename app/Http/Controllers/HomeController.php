43ff8503f32519a4f9442d376c3e87a60373a889
taku <test@test.com>
2018-11-20 18:48:43 +0900

    upd 2018.11.20-1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,30 @@ class HomeController extends Controller
      */
     public function index()
     {
+//        $message = ['a' => 1, 'b' => 2, 'c' => 3];
+        $message = ['test'];
+
+        //storage/logs/laravel.log に出力
+        // DEBUG
+        logger($message);
+        logger()->debug($message);
+        // INFO
+        info($message);
+        logger()->info($message);
+        // NOTICE
+        logger()->notice($message);
+        // WARNING
+        logger()->warning($message);
+        // ERROR
+        logger()->error($message);
+        // CRITICAL
+        logger()->critical($message);
+        // ALERT
+        logger()->alert($message);
+        // EMERGENCY
+        logger()->emergency($message);
+
+        
         return view('home');
     }
 }
---
+++ app/Http/Controllers/Omo/HudaController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Omo;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Huda;
+use Excel;
+
+class HudaController extends Controller
+{
+    protected $huda = null;
+
+    public function __construct(Huda $huda)
+    {
+        $this->huda = $huda;
+    }
+
+    public function index()
+    {
+        $data = [];
+        $data['huda'] = $this->huda->all();
+        return view('huda', $data);
+    }
+
+
+    /**
+     * CSVインポート
+     *
+     * @param  Request
+     * @return \Illuminate\Http\Response
+     */
+     public function import(Request $request)
+     {
+
+         $file = $request->file('csv_file');
+         $reader = Excel::load($file->getRealPath());
+
+         $rows = $reader->toArray();
+
+         foreach ($rows as $row){
+             // isset(x) x = nullの場合、False x = null以外の場合、True
+             if (!isset($row['huda_name'])) {
+                 return redirect()->back();
+             }
+
+             // 指定したオブジェクトが存在する場合、first()で取得(最初のれこーdお取得
+             // 存在しなければインスタンス化する
+             $record = $this->huda->firstOrNew(['huda_name'  => $row['huda_name'],'created_at' => $row['created_at']]);
+//           $record = $this->huda->firstOrNew(['huda_name' => $row['huda_name']]);
+//           $record = new huda();
+             
+             // 読み込んだCSVのレコードの各列の値を、オブジェクトに設定
+             $record->huda_name = $row['huda_name'];
+//             $record->created_at = date('Y/m/d H:i',strtotime($row['created_at']));
+           $record->created_at = $row['created_at'];
+             // DBにinsert
+             $record->save();
+         }
+
+        // ★ 以下の2パターンとも、web.phpに、getメソッドを用意する必要あり（解決策はないか・・・）
+        //        return redirect()->route('huda');
+         return redirect()->action('Omo\HudaController@index');
+     } 
+    
+    
+    
+}
